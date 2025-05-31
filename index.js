cfbee1f7cc4773f79ef850df1ec167c1b27b822f
Tetiana5Buria <poriniya@gmail.com>
2025-05-31 17:19:44 +0300

    Test commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* const test = document.getElementById("test");
+console.log(test); // Повертає елемент з ID "test"
+return; */
+
+/* import { log } from "neo-async"; */
+const a = 6;
+const b = 3;
+if (a > b) {
+    console.log("Hello world!"); // Виведе "Hello world" у консоль;
+}
+
+
+
